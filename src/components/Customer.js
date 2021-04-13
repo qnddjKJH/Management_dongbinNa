166b0a2acf73f1ba25a4b2aa291e40d2f7d13b5d
qnddjKJH <qnddjKJH.dev@gmail.com>
2021-04-13 14:16:56 +0900

    Init Project

---
+++ src/components/Customer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Customer extends React.Component {
+    
+    render() {
+        return( 
+            <div> 
+                <h2>홍길동</h2>
+                <p>950406</p>
+                <p>남자</p>
+                <p>대학생</p>
+            </div>
+        );
+    }
+}
+
+export default Customer;
