914a974d87801acb88b345969c801ecad6dd542d
qnddjKJH <qnddjKJH.dev@gmail.com>
2021-04-20 14:38:06 +0900

    Added express server & config Babel

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/hello", (req, res) => {
+    console.log("hello");
+});
+
+app.listen(port, () => {
+    console.log(`Listening on PORT ${port}`);
+})
+ 
